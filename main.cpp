9e17fffbfb72f17163a5657c480f59213d93be4b
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-30 14:34:16 +0800

    add useful_header_file.h

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include "useful_header_file.h"
 
 using namespace std;
 
---
+++ useful_header_file.h
@@ -0,0 +1,39 @@
+//
+// Created by VULCA on 30/5/2024.
+//
+
+#ifndef GIT_HELLO_WORLD_USEFUL_HEADER_FILE_H
+#define GIT_HELLO_WORLD_USEFUL_HEADER_FILE_H
+
+#endif //GIT_HELLO_WORLD_USEFUL_HEADER_FILE_H
+
+#include "iostream"      // 标准输入输出流
+#include "iomanip"       // 输入输出流格式化
+#include "fstream"       // 文件输入输出
+
+#include "string"        // 字符串类及其函数
+
+#include "vector"        // 动态数组
+#include "list"          // 双向链表
+#include "deque"         // 双端队列
+#include "array"         // 定长数组
+#include "map"           // 有序映射
+#include "unordered_map" // 无序映射
+#include "set"           // 有序集合
+#include "unordered_set" // 无序集合
+
+#include "algorithm"     // 算法函数
+#include "iterator"      // 迭代器
+
+#include "utility"       // 实用工具函数
+#include "functional"    // 函数对象及其支持
+
+#include "cmath"         // 数学函数
+#include "cstdlib"       // 常用实用函数
+
+#include "ctime"         // 时间和日期函数
+#include "chrono"        // 高精度时间测量
+
+#include "thread"        // 线程支持
+#include "mutex"         // 互斥锁支持
+#include "future"        // 异步操作支持
